2bdce085a23ce7b5e37f33dd60b61065227e28b3
Wynn Netherland <wynn.netherland@gmail.com>
2010-01-06 22:06:52 -0600

    Added skeleton tests

--- test/test_chunky_baconfile.rb
+++ test/test_chunky_baconfile.rb
@@ -1,7 +1,40 @@
 require 'helper'
 
 class TestChunkyBaconfile < Test::Unit::TestCase
-  should "probably rename this file and start testing for real" do
-    flunk "hey buddy, you should probably rename this file and start testing for real"
+  context "when unauthenticated" do
+
+    should_eventually "return info about a folder" do
+      
+    end
+    
+    should_eventually "return info about a file" do
+      
+    end
+    
+    should_eventually "return a list of most recent public files" do
+      
+    end
   end
+  
+  context "when authenticated" do
+
+    should_eventually "verify credentials" do
+      
+    end
+
+    should_eventually "create a new folder" do
+      
+    end
+    
+    should_eventually "creata a new file" do
+      
+    end
+    
+    should_eventually "delete a file or a folder" do
+      
+    end
+    
+  end
+  
+  
 end
