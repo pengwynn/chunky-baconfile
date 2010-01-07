9a9c33ea06fbbbf00ac37b1c11dccd58c2fb5638
Wynn Netherland <wynn.netherland@gmail.com>
2010-01-06 22:40:31 -0600

    Test helper setup

--- test/helper.rb
+++ test/helper.rb
@@ -1,10 +1,29 @@
 require 'rubygems'
 require 'test/unit'
 require 'shoulda'
+require 'fakeweb'
+
+FakeWeb.allow_net_connect = false
 
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'chunky_baconfile'
 
 class Test::Unit::TestCase
+end
+
+def baconfile_url(url)
+  url =~ /^http/ ? url : "http://baconfile.com#{url}"
+end
+
+def fixture_file(filename)
+  return '' if filename == ''
+  file_path = File.expand_path(File.dirname(__FILE__) + '/fixtures/' + filename)
+  File.read(file_path)
+end
+
+def stub_get(url, filename, options={})
+  opts = {:body => fixture_file(filename)}.merge(options)
+  
+  FakeWeb.register_uri(:get, baconfile_url(url), opts)
 end
--- test/test_chunky_baconfile.rb
+++ test/test_chunky_baconfile.rb
@@ -2,9 +2,17 @@ require 'helper'
 
 class TestChunkyBaconfile < Test::Unit::TestCase
   context "when unauthenticated" do
+    
+    setup do
+      @bacon = ChunkyBaconfile.new("leah")
+    end
 
-    should_eventually "return info about a folder" do
-      
+    should "return items in a folder" do
+      stub_get("http://baconfile.com/leah/bacon.json", "bacon.json")
+      items = @bacon.folder("bacon")
+      items.size.should == 11
+      items.first.size.should == 84295
+      items.first.user.username.should == 'leah'
     end
     
     should_eventually "return info about a file" do
